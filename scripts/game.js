290cc9e4b09e37392d958789b077eb778dcb45c7
Piotr Kowalski <piotr.kowalski@redefine.pl>
2013-08-07 08:29:10 +0200

    add secure to not reflow when cound't do this

--- scripts/game.js
+++ scripts/game.js
@@ -5,31 +5,54 @@
     var utils = (global.utils = global.utils || {});
 
     function setScale(evt) {
+        /**
+         * No small, smallest version.
+         */
+        if (evt.wheelDelta < 0 && snooker.SCALE === setScale.SMALLEST_SCALE) {
+            return false;
+        }
+
+        /**
+         * No zoom, the biggest version.
+         */
+        if (evt.wheelDelta > 0 && snooker.SCALE === setScale.BIGGEST_SCALE) {
+            return false;
+        }
+
+        /**
+         * Change SCALE value.
+         * @type {number}
+         */
         snooker.SCALE += 0.05 * (evt.wheelDelta / 10);
 
         /**
-         * Secure for not too small.
+         * Secure for change SCALE not too small.
          */
-        if (snooker.SCALE < 1) {
-            snooker.SCALE = 1;
+        if (snooker.SCALE < setScale.SMALLEST_SCALE) {
+            snooker.SCALE = setScale.SMALLEST_SCALE;
         }
 
         /**
-         * Secure for not too big.
+         * Secure for change SCALE not too big.
          */
-        if (snooker.SCALE > 3) {
-            snooker.SCALE = 3;
+        if (snooker.SCALE > setScale.BIGGEST_SCALE) {
+            snooker.SCALE = setScale.BIGGEST_SCALE;
         }
+
+        return true;
     }
+
+    setScale.SMALLEST_SCALE = 1;
+    setScale.BIGGEST_SCALE = 3;
 
     utils.listener.add(window, "load", function () {
         snooker.init();
     });
 
     utils.listener.add(document, "mousewheel", function (evt) {
-        setScale(evt);
-
-        snooker.init();
+        if (setScale(evt)) {
+            snooker.init();
+        }
 
         evt.preventDefault();
         evt.stopPropagation();
