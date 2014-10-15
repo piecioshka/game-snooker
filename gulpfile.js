c989fb65814136a52f0f85c52e331e3fb0312b60
Piotr Kowalski <piecioshka@gmail.com>
2014-10-15 19:27:48 +0200

    Support GA.

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,38 @@
 /*global require */
 
-'use strict';
+(function (root) {
+    'use strict';
 
-var _ = require('underscore');
-var rjs = require('requirejs');
-var gulp = require('gulp');
-var clean = require('gulp-clean');
-var Q = require('q');
-// Konfiguracja aplikacji potrzebna do budowy dist/
-var config = require('./config.json');
+    var _ = require('underscore');
+    var rjs = require('requirejs');
+    var gulp = require('gulp');
+    var clean = require('gulp-clean');
+    var Q = require('q');
 
-gulp.task('prepare', function () {
-    // usuwamy katalog /dist
-    return gulp.src('dist/', { read: false })
-        .pipe(clean({ force: true }));
-});
+    // Konfiguracja aplikacji potrzebna do budowy dist/
+    var config = require('./config.json');
 
-gulp.task('build', ['prepare'], function () {
-    // optymalizacja r.js
-    var deferred = Q.defer();
-    rjs.optimize(config, function () {
-        deferred.resolve();
+    gulp.task('prepare', function () {
+        // usuwamy katalog /dist
+        return gulp.src('dist/', { read: false })
+            .pipe(clean({ force: true }));
     });
-    return deferred.promise;
-});
 
-gulp.task('clean', ['build'], function () {
-    // usuwamy logi builda
-    return gulp.src("./dist/build.txt", { read: false })
-        .pipe(clean());
-});
+    gulp.task('build', ['prepare'], function () {
+        // optymalizacja r.js
+        var deferred = Q.defer();
+        rjs.optimize(config, function () {
+            deferred.resolve();
+        });
+        return deferred.promise;
+    });
 
-gulp.task('default', ['prepare', 'build', 'clean']);
+    gulp.task('clean', ['build'], function () {
+        // usuwamy logi builda
+        return gulp.src("./dist/build.txt", { read: false })
+            .pipe(clean());
+    });
+
+    gulp.task('default', ['prepare', 'build', 'clean']);
+
+}(this));
